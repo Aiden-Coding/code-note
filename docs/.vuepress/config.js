9e36dd29a2420f4116f4e057479faf57239de8d5
dong <dwx_job@163.com>
2023-12-19 00:51:54 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,8 @@ module.exports = {
         subSidebar: 'auto',
         nav: [
             { text: '首页', link: '/' },
-            { text: 'flowable', link: '/flowable/' }
+            { text: 'flowable', link: '/flowable/Flowable课件-基础篇' },
+            { text: 'test', link: '/language/' }
         ], sidebar: {
             '/flowable/': [
                 {
@@ -20,6 +21,36 @@ module.exports = {
                     collapsable: true,
                     children: ['Flowable课件-基础篇', 'Flowable课件-高级篇']
                 }
+            ],
+            '/language/': [
+                //Group1
+                {
+                    title: 'Group1',
+                    children: [
+                        'c'
+                        // {
+                        //     title: 'chinese',   // 必要的
+                        //     children: ['c']
+                        // }
+                    ]
+                },
+                //Group2
+                {
+                    title: 'Group2',
+                    children: [
+                        //小组A
+                        {
+                            title: 'A',
+                            children: ['a'],
+                        },
+                        //小组B
+                        {
+                            title: 'B',
+                            children: ['b'],
+                        },
+                    ],
+                },
+
             ],
             '/': [''] //不能放在数组第一个，否则会导致右侧栏无法使用
         },
