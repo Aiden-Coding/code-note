4cc8869c95567c9a50156cea5dc8bf3a8238625d
dong <dwx_job@163.com>
2023-12-19 00:33:13 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     base: '/code-note/',
-    title: '...',
+    title: '学习笔记',
     description: '...',
     theme: 'reco',
     locales: {
@@ -12,32 +12,17 @@ module.exports = {
         subSidebar: 'auto',
         nav: [
             { text: '首页', link: '/' },
-            {
-                text: '冴羽的 JavaScript 博客',
-                items: [
-                    { text: 'Github', link: 'https://github.com/mqyqingfeng' },
-                    { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
-                ]
-            }
-        ], sidebar: [
-            {
-                title: '欢迎学习',
-                path: '/',
-                collapsable: false, // 不折叠
-                children: [
-                    { title: "学前必读", path: "/" }
-                ]
-            },
-            {
-                title: "基础学习",
-                path: '/handbook/ConditionalTypes',
-                collapsable: false, // 不折叠
-                children: [
-                    { title: "条件类型", path: "/handbook/ConditionalTypes" },
-                    { title: "泛型", path: "/handbook/Generics" }
-                ],
-            }
-        ]
+            { text: 'flowable', link: '/flowable/' }
+        ], sidebar: {
+            '/flowable/': [
+                {
+                    title: 'flowalbe 学习',
+                    collapsable: true,
+                    children: ['Flowable课件-基础篇', 'Flowable课件-高级篇']
+                }
+            ],
+            '/': [''] //不能放在数组第一个，否则会导致右侧栏无法使用
+        },
 
     }
 }
