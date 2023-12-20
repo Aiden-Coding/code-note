0c6664ef790108f53bfe9b7cc047698be1143a2a
dong <dwx_job@163.com>
2023-12-20 21:29:56 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,9 @@ module.exports = {
         nav: [
             { text: "首页", link: "/" },
             { text: "flowable", link: "/flowable/Flowable课件-基础篇" },
-            { text: "others", title: "其他", link: "/cs/others/面向对象思想" }
+            { text: "操作系统", link: "/cs/system/Linux" },
+            { text: "计算机网络", link: "/cs/net/HTTP" },
+            { text: "其他", link: "/cs/others/面向对象思想" }
         ],
         sidebar: {
             "/flowable/": [
@@ -36,6 +38,27 @@ module.exports = {
                     title: "其他",
                     children: [
                         '代码风格规范', '代码可读性', '分布式', '攻击技术', '集群', '面向对象思想', '正则表达式'
+                    ]
+                }
+            ],
+            "/cs/system/": [
+                "Linux",
+                {
+                    title: "计算机操作系统",
+                    collapsable: false,
+                    children: [
+                        '计算机操作系统 - 目录', '计算机操作系统 - 概述', '计算机操作系统 - 进程管理', '计算机操作系统 - 链接', '计算机操作系统 - 内存管理', '计算机操作系统 - 设备管理', '计算机操作系统 - 死锁', '计算机操作系统'
+                    ]
+                }
+            ],
+            "/cs/net/": [
+                "HTTP",
+                "Socket",
+                {
+                    title: "计算机网络",
+                    collapsable: false,
+                    children: [
+                        '计算机网络 - 目录', '计算机网络 - 传输层', '计算机网络 - 概述', '计算机网络 - 链路层', '计算机网络 - 网络层', '计算机网络 - 物理层', '计算机网络 - 应用层', '计算机网络'
                     ]
                 }
             ]
