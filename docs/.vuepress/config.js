2f2ebcab9772c54bafeac8a4eef32a55527f376d
dong <dwx_job@163.com>
2024-01-04 01:22:03 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -93,7 +93,7 @@ export default {
             },
             { text: "bestJavaer", link: "/bestJavaer/" },
             { text: "JavaGuide", link: "/JavaGuide/" },
-            { text: "CodeGuide", link: "/CodeGuide/" },
+            { text: "CodeGuide", link: "/CodeGuide/home.md" },
             { text: "其他", link: "/cs/others/面向对象思想.md" },
         ],
         series: {
