698cc013f39f5a4adb666335cce93d5e7387f7d2
dong <dwx_job@163.com>
2024-01-04 00:42:55 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,34 +37,34 @@ export default {
           大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
                     style: "font-size: 12px;",
                 },
-                { type: "hr" },
-                { type: "title", content: "QQ 群" },
-                {
-                    type: "text",
-                    content: `
-          <ul>
-            <li>QQ群1：1037296104</li>
-            <li>QQ群2：1061561395</li>
-            <li>QQ群3：962687802</li>
-          </ul>`,
-                    style: "font-size: 12px;",
-                },
-                { type: "hr" },
-                { type: "title", content: "GitHub" },
-                {
-                    type: "text",
-                    content: `
-          <ul>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco/issues">Issues<a/></li>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco/discussions/1">Discussions<a/></li>
-          </ul>`,
-                    style: "font-size: 12px;",
-                },
-                { type: "hr" },
-                {
-                    type: "buttongroup",
-                    children: [{ text: "打赏", link: "/docs/others/donate.html" }],
-                },
+                //         { type: "hr" },
+                //         { type: "title", content: "QQ 群" },
+                //         {
+                //             type: "text",
+                //             content: `
+                //   <ul>
+                //     <li>QQ群1：1037296104</li>
+                //     <li>QQ群2：1061561395</li>
+                //     <li>QQ群3：962687802</li>
+                //   </ul>`,
+                //             style: "font-size: 12px;",
+                //         },
+                //         { type: "hr" },
+                //         { type: "title", content: "GitHub" },
+                //         {
+                //             type: "text",
+                //             content: `
+                //   <ul>
+                //     <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco/issues">Issues<a/></li>
+                //     <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco/discussions/1">Discussions<a/></li>
+                //   </ul>`,
+                //             style: "font-size: 12px;",
+                //         },
+                //         { type: "hr" },
+                //         {
+                //             type: "buttongroup",
+                //             children: [{ text: "打赏", link: "/docs/others/donate.html" }],
+                //         },
             ],
         },
         subSidebar: "auto",
@@ -91,6 +91,7 @@ export default {
                     { text: "bulletin-popover", link: "/toBeBetterJavaer/bulletin-popover" },
                 ],
             },
+            { text: "bestJavaer", link: "/bestJavaer/" },
             { text: "其他", link: "/cs/others/面向对象思想.md" },
         ],
         series: {
