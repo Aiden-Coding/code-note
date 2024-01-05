8ea105d9be9dc473ce7198b27657965889441f3b
dong <dwx_job@163.com>
2024-01-05 21:54:17 +0800

    1

---
+++ docs/.vuepress/navbar.js
@@ -0,0 +1,30 @@
+export const navbar = [
+    { text: "首页", link: "/" },
+    { text: "Java", link: "/cs/java/Java 基础.md" },
+    { text: "操作系统", link: "/cs/system/Linux.md" },
+    { text: "计算机网络", link: "/cs/net/HTTP.md" },
+    { text: "数据库", link: "/cs/database/MySQL.md" },
+    { text: "算法", link: "/cs/algo/算法 - 目录.md" },
+    { text: "设计模式", link: "/cs/design/设计模式 - 目录.md" },
+    { text: "flowable", link: "/flowable/Flowable课件-基础篇.md" },
+    { text: "代码随想录", link: "/cs/programmercarl/" },
+    { text: "微服务", link: "/cs/cloud/微服务和Spring Cloud Alibaba介绍.md" },
+    { text: "advanced-java", link: "/advanced-java/big-data/" },
+    {
+        text: "toBeBetterJavaer",
+        children: [
+            { text: "home", link: "/toBeBetterJavaer/home.md" },
+            { text: "comments", link: "/toBeBetterJavaer/comments" },
+            { text: "vue-previews", link: "/toBeBetterJavaer/vue-previews" },
+            { text: "bulletin-popover", link: "/toBeBetterJavaer/bulletin-popover" },
+        ],
+    },
+    { text: "bestJavaer", link: "/bestJavaer/README.md" },
+    { text: "JavaGuide", link: "/JavaGuide/README.md" },
+    {
+        text: "CodeGuide",
+        link: "/CodeGuide/home.md",
+    },
+    { text: "toBeTopJavaer", link: "/toBeTopJavaer/menu.md" },
+    { text: "其他", link: "/cs/others/面向对象思想.md" },
+]
---
+++ docs/.vuepress/series/flowable.js
@@ -0,0 +1,7 @@
+export const flowable = [
+    {
+        text: "flowalbe 学习",
+        collapsable: true,
+        children: ["Flowable课件-基础篇.md", "Flowable课件-高级篇.md"],
+    },
+]
